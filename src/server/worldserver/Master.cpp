9939fb98224dd8162790867f75bfdc1b38492c66
DarkSoe <D.Angel4@gmx.net>
2011-09-01 06:00:23 +0200

    worldserver/Master.cpp ALive Core Logo

--- src/server/worldserver/Master.cpp
+++ src/server/worldserver/Master.cpp
@@ -126,15 +126,13 @@ int Master::Run()
     sLog->outString("%s (worldserver-daemon)", _FULLVERSION);
     sLog->outString("<Ctrl-C> to stop.\n");
 
-    sLog->outString(" ______                       __");
-    sLog->outString("/\\__  _\\       __          __/\\ \\__");
-    sLog->outString("\\/_/\\ \\/ _ __ /\\_\\    ___ /\\_\\ \\ ,_\\  __  __");
-    sLog->outString("   \\ \\ \\/\\`'__\\/\\ \\ /' _ `\\/\\ \\ \\ \\/ /\\ \\/\\ \\");
-    sLog->outString("    \\ \\ \\ \\ \\/ \\ \\ \\/\\ \\/\\ \\ \\ \\ \\ \\_\\ \\ \\_\\ \\");
-    sLog->outString("     \\ \\_\\ \\_\\  \\ \\_\\ \\_\\ \\_\\ \\_\\ \\__\\\\/`____ \\");
-    sLog->outString("      \\/_/\\/_/   \\/_/\\/_/\\/_/\\/_/\\/__/ `/___/> \\");
-    sLog->outString("                                 C O R E  /\\___/");
-    sLog->outString("http://TrinityCore.org                    \\/__/\n");
+    sLog->outString(" _______";
+	sLog->outString("/\      \\    \\          \\                  _______");
+    sLog->outString("\ \      \\    \\                \\    //     \\    \\");
+    sLog->outString(" \ \______\\    \\         \\     \\  //       \\____\\");
+    sLog->outString("  \ \      \\    \\_____    \\     \\//         \\_______      C O R E");
+	sLog->outString(" ");
+	sLog->outString("http://wow-alive.de/");
 
 #ifdef USE_SFMT_FOR_RNG
     sLog->outString("\n");
