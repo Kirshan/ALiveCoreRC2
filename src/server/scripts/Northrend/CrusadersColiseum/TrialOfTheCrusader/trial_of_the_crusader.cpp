4c38fd6d1af921dde6e73988475c2452ea31b28f
DarkSoe <D.Angel4@gmx.net>
2011-09-04 03:50:33 +0200

    [PDK/EndEvent] add mage is casting Portal to Dalaran

--- src/server/scripts/Northrend/CrusadersColiseum/TrialOfTheCrusader/trial_of_the_crusader.cpp
+++ src/server/scripts/Northrend/CrusadersColiseum/TrialOfTheCrusader/trial_of_the_crusader.cpp
@@ -797,9 +797,28 @@ class npc_tirion_toc : public CreatureScript
                             break;
                         case 6005:
                             DoScriptText(SAY_STAGE_4_06, me);
-                            m_uiUpdateTimer = 20000;
-                            m_pInstance->SetData(TYPE_EVENT, 6010);
+							me->SummonCreature(33643, me->GetPositionX(), me->GetPositionY()+5, me->GetPositionZ(), me->GetOrientation());
+                            m_uiUpdateTimer = 10000;
+                            m_pInstance->SetData(TYPE_EVENT, 6008);
                             break;
+						case 6008:
+						{
+							Creature *m_mage = me->FindNearestCreature(33643, 100.0f, true);
+							m_mage->SetOrientation(me->GetOrientation());
+							m_mage->AI()->DoCastAOE(53142);
+                            m_uiUpdateTimer = 10000;
+                            m_pInstance->SetData(TYPE_EVENT, 6009);
+                            break;
+						}
+						case 6009:
+						{
+							Creature *m_mage = me->FindNearestCreature(33643, 100.0f, true);
+							GameObject *portal = me->FindNearestGameObject(191164, 100.0f);
+							portal->DestroyForNearbyPlayers();
+							m_mage->SummonGameObject(191164, 787.932556f, 138.289780f, 142.612152f,0,0,0,0,0,6000000);
+							m_uiUpdateTimer = 1000;
+                            m_pInstance->SetData(TYPE_EVENT, 6009);
+						}
                         case 6010:
                             if (IsHeroic())
                             {
