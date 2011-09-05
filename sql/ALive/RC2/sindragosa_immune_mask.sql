04d72b6ea81f6571498c9b2d5579bfa777ef90ad
DarkSoe <D.Angel4@gmx.net>
2011-09-05 05:10:05 +0200

    [SQL] Fix a fail in Sindra imunity sql

--- sql/ALive/RC2/sindragosa_immune_mask.sql
+++ sql/ALive/RC2/sindragosa_immune_mask.sql
@@ -1,2 +1,2 @@
 -- Sindragosa cc immun machen
-UPDATE `creature_template` SET `mechanic_immune_maske` = '617299803' WHERE `entry` = '36583';
+UPDATE `creature_template` SET `mechanic_immune_mask` = '617299803' WHERE `entry` IN ('36853', '38265', '38266', '38267');
