b5fba70a6535c2d19b8073a9d4462dc50967e1d2
DarkSoe <D.Angel4@gmx.net>
2011-09-11 02:14:08 +0200

    [Ulduar/Razorscale] rewritten

--- src/server/scripts/Northrend/Ulduar/ulduar/boss_razorscale.cpp
+++ src/server/scripts/Northrend/Ulduar/ulduar/boss_razorscale.cpp
@@ -1,5 +1,6 @@
 /*
 * Copyright (C) 2008-2011 TrinityCore <http://www.trinitycore.org/>
+* Copyright (C) 2008-2011 ALiveCore <http://www.wow-alive.de/>
 *
 * This program is free software; you can redistribute it and/or modify it
 * under the terms of the GNU General Public License as published by the
